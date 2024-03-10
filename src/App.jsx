67cedf2db2f9a218f2dd4dd269415329211b0b3a
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-10 12:56:46 +0100

    add: output cards and serachBar wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,93 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const params = {
+      pageSize: 20,
+      page: page,
+      count: 20,
+    };
+    const fetchData = () => {
+      axios
+        .get(`https://api.magicthegathering.io/v1/cards?`, { params })
+        .then((res) => {
+          setCards(res.data.cards);
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching cards:", error);
+        });
+    };
+    fetchData();
+  }, [page]);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
   return (
     <>
-    <div className="card bg-light d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-5 mx-auto mt-5" style={{height:'300px'}}>
-      <h4>My react and bootstrap template</h4>
-    </div>
+      <div className="container my-5">
+        <div className="d-flex gap-4 m-4 align-items-center justify-content-between">
+          <div className="d-flex gap-4 m-4 align-items-center justify-content-center">
+            <button
+              className="btn btn-primary"
+              onClick={prevPage}
+              disabled={page === 1}
+            >
+              prev
+            </button>
+            <p className="m-0">Page: {page}</p>
+            <button className="btn btn-primary" onClick={nextPage}>
+              next
+            </button>
+          </div>
+          <div className="d-flex gap-3 align-items-center">
+            <label htmlFor="">search</label>
+            <input
+              className="form-control"
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center align-items-center">
+          {cards.map((card) =>
+            card.imageUrl ? (
+              <div
+                key={card.id}
+                className="col-12 col-md-6 col-lg-3 d-flex justify-content-center g-3"
+              >
+                <div
+                  className="text-center card bg-light d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-5 mt-5 mx-auto"
+                  style={{ minHeight: "300px" }}
+                >
+                  <img src={card.imageUrl} alt="" />
+                  <div className="py-3">
+                    <p className="fw-bold m-0">{card.name}</p>
+                    <p className="m-0"> {card.type}</p>
+                  </div>
+                </div>
+              </div>
+            ) : null
+          )}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Header from './assets/components/Header.jsx'
+import Header from './components/Header.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
