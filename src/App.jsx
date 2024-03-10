a1784ad95c09112fac86121d8e0fb38ede49881c
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-10 15:59:38 +0100

    add: keyup enter to search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,28 @@ function App() {
   const [cards, setCards] = useState([]);
   const [page, setPage] = useState(1);
   const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const params = {
       pageSize: 20,
       page: page,
       count: 20,
-      name:inputValue,
+      name: inputValue,
+      contains: "imageUrl",
     };
     const fetchData = () => {
       axios
         .get(`https://api.magicthegathering.io/v1/cards?`, { params })
         .then((res) => {
           setCards(res.data.cards);
-          console.log(res.data);
+          console.log(res.data.cards);
         })
         .catch((error) => {
           console.error("Error fetching cards:", error);
-        });
+        })
+        .finally(() => setLoading(false));
     };
     fetchData();
   }, [page, inputValue]);
@@ -41,8 +45,9 @@ function App() {
   return (
     <>
       <div className="container my-5">
-        <div className="d-flex gap-4 m-4 align-items-center justify-content-between">
-          <div className="d-flex gap-4 m-4 align-items-center justify-content-center">
+        <div className="row gap-4 m-4 align-items-center justify-content-between">
+          {/* pagination */}
+          <div className="d-flex gap-4 m-4 align-items-center justify-content-center col-12 col-md-6 col-lg-4">
             <button
               className="btn btn-primary"
               onClick={prevPage}
@@ -51,40 +56,103 @@ function App() {
               prev
             </button>
             <p className="m-0">Page: {page}</p>
-            <button className="btn btn-primary" onClick={nextPage}>
+            <button
+              className="btn btn-primary"
+              onClick={nextPage}
+              disabled={cards.length < 20}
+            >
               next
             </button>
           </div>
-          <div className="d-flex gap-3 align-items-center">
+          <div className="col-12 col-md-10 col-lg-4 d-flex gap-3 align-items-center">
             <label htmlFor="">search</label>
             <input
               className="form-control"
               type="text"
               value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
+              onChange={(e) => setInputValue()}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  setInputValue(e.target.value);
+                  setPage(1);
+                }
+              }}
             />
           </div>
         </div>
-        <div className="row justify-content-start align-items-center g-5">
-          {cards.map((card) =>
-            card.imageUrl ? (
-              <div
-                key={card.id}
-                className="col-12 col-md-6 col-lg-3 d-flex justify-content-center"
-              >
+        {loading && (
+          <div
+            className="d-flex justify-content-center text-primary align-items-center" /* style={{height:'30vh'}} */
+          >
+            <div
+              className="spinner-border"
+              role="status"
+              style={{ height: "4rem", width: "4rem" }}
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
+
+        {/* card */}
+        {!loading && (
+          <div className="row justify-content-start align-items-center g-5">
+            {cards.map((card) =>
+              card.imageUrl ? (
                 <div
-                  className=" w-100 py-5 text-center card bg-light d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-5 mx-auto"
-                  style={{ minHeight: "300px" }}
+                  key={card.id}
+                  className="col-12 col-md-6 col-lg-3 d-flex justify-content-center"
                 >
-                  <img src={card.imageUrl} alt="" />
-                  <div className="py-3">
-                    <p className="fw-bold m-0">{card.name}</p>
-                    <p className="m-0"> {card.type}</p>
+                  <div
+                    className=" w-100 py-5 text-center card bg-light d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-5 mx-auto"
+                    style={{ height: "450px" }}
+                  >
+                    <img
+                      style={{ width: "200px" }}
+                      src={card.imageUrl}
+                      alt=""
+                    />
+                    <div className="py-3 px-2 ms_font-size">
+                      <p className="fw-bold m-0">{card.name}</p>
+                      <p className="m-0">
+                        <strong>Type:</strong> {card.type}
+                      </p>
+                      <p className="m-0">
+                        <strong>Rarity:</strong> {card.rarity}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : null
-          )}
+              ) : null
+            )}
+          </div>
+        )}
+        {/* card */}
+
+        {cards.length === 0 && !loading && (
+          <div className="text-center mt-5 pt-5">
+            <p className="fw-bold fs-3">no results found</p>
+          </div>
+        )}
+        <div className="row gap-4 m-4 align-items-center justify-content-between">
+          {/* pagination */}
+          <div className="d-flex gap-4 m-4 align-items-center justify-content-center col-12 col-md-6 col-lg-4">
+            <button
+              className="btn btn-primary"
+              onClick={prevPage}
+              disabled={page === 1}
+            >
+              prev
+            </button>
+            <p className="m-0">Page: {page}</p>
+            <button
+              className="btn btn-primary"
+              onClick={nextPage}
+              disabled={cards.length < 20}
+            >
+              next
+            </button>
+          </div>
         </div>
       </div>
     </>
