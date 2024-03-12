6a139ee13250df45aab8f3c22b007b3c15d7b65f
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-12 19:48:34 +0100

    remove e.target.value

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,7 @@ function App() {
               type="text"
               value={inputValue}
               placeholder="Simple search"
-              onChange={(e) => setInputValue(e.target.value)}
+              onChange={(e) => setInputValue()}
               onKeyUp={(e) => {
                 if (e.key === "Enter") {
                   setInputValue(e.target.value);
